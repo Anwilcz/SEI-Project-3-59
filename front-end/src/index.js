39a6726e1269c77d20d04e0aca380e0e21f3795d
Anna Wilczynska <anna.wilcz@outlook.com>
2022-01-03 18:09:01 +0000

    Restructured

--- front-end/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/main.css'
-import 'semantic-ui-css/semantic.min.css'
-import App from './App'
-import 'mapbox-gl/dist/mapbox-gl.css'
-ReactDOM.render(<App />, document.getElementById('root'))
